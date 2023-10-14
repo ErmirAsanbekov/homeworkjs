e4915dd84897a67b7c4dfaf2a915ad8aca9da4eb
Asanbeko <ermirasanbekov63@gmail.com>
2023-10-14 21:09:32 +0600

    add files

---
+++ homeworkj/main.js
@@ -0,0 +1,19 @@
+let имя = "ERMIR"
+let возраст = "15"
+let город = "BISHKEK"
+
+console.log(имя)
+console.log(возраст)
+console.log(город)
+
+const любимое_число = "12"
+const любимый_цвет = "черный"
+
+console.log(любимое_число )
+console.log( любимый_цвет)
+
+const первое_слово = "Hello"
+const второе_слово = "   world"
+
+console.log(первое_слово + второе_слово)
+
---
+++ homeworkj2/main.js
@@ -0,0 +1,48 @@
+let age = (15 > 18) ? "Совершеннолетний" : "Несовершеннолетний"
+
+if (15 > 18) {
+    age = "Несовершеннолетний"
+} else (15 < 18) 
+{
+"Совершеннолетний"
+}
+
+console.log(age)
+
+const isStudent = Boolean(true)
+
+
+console.log(isStudent)
+
+let num1 = 100
+let num2 = 250
+
+console.log(100 + 250)
+
+const day = '1,2,3,4,5,6,7'
+   
+ console.log(' понедельник')  
+ console.log(' вторник')
+ console.log('среда')
+ console.log(' четверг')
+ console.log(' пятница')
+ console.log(' субота')
+ console.log(' воскресенье')
+
+
+const temperature = '+3°'
+
+
+
+const num = "155"
+
+
+
+   
+
+   console.log(temperature)
+
+
+   const score = '0,10,20,30,40,50,60,70,80,90,100'
+   console.log('0')
+   console.log('10')
