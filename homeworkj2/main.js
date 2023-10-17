6da4fa011aec1d078bb3b20baf87cb7ff8bee5b4
Asanbeko <ermirasanbekov63@gmail.com>
2023-10-17 20:52:42 +0600

    first lessons

--- homeworkj2/main.js
+++ homeworkj2/main.js
@@ -1,18 +1,17 @@
-let age = (15 > 18) ? "Совершеннолетний" : "Несовершеннолетний"
+let age = "15"
 
-if (15 > 18) {
-    age = "Несовершеннолетний"
-} else (15 < 18) 
-{
-"Совершеннолетний"
-}
-
-console.log(age)
-
-const isStudent = Boolean(true)
+"15" > "18" ? console.log("Cовершеннолетний") : console.log(" Несовершеннолетний")
 
 
-console.log(isStudent)
+
+
+
+const isStudent = name
+
+name = "не студент"
+
+
+console.log(name)
 
 let num1 = 100
 let num2 = 250
@@ -34,15 +33,110 @@ const temperature = '+3°'
 
 
 
-const num = "155"
-
-
-
-   
-
    console.log(temperature)
 
 
    const score = '0,10,20,30,40,50,60,70,80,90,100'
-   console.log('0')
-   console.log('10')
+ 
+console.log('60' > '100' ? "не сдал" : "сдал")
+ 
+   
+let num = 50
+
+num = "Четное"
+
+console.log(num)
+
+const language = "Python" 
+
+console.log(language)
+
+
+// const userRole = "admin"
+
+// if (admin ) {
+//     console.log('Доступ разрешен');
+// }
+
+//  else if ( admin) {
+//     console.log('Доступ запрешен')
+//  }
+
+let  a = 149
+let  b = 275
+let  c = 178
+
+if (b < a) {
+console.log(b)
+getMaxOfArray([178, 149, 275])
+}
+    console.log(a)
+    console.log(b)
+    console.log(c)
+
+    let fruit = 'Яблоко' 
+
+    fruit = 'красный'
+
+console.log(fruit)
+
+
+const grade = 'A, B, C, D, F'
+
+switch (grade) {
+    case 'Apple':
+    console.log(grade)
+}
+
+let password = '1784933435'
+
+password = 'Пароль надежный'
+
+console.log(password)
+
+let x = 1
+let y = 1
+
+
+
+console.log( "x и y равны")
+
+const isRaining = true
+
+
+console.log( "Дождя нет")
+
+const isLeapYear = false
+
+console.log("Год не високосный")
+
+let userInput = ''
+
+console.log("Пустая строка")
+
+let numb = '3'
+
+if (3 < 10) {
+    console.log(numb);
+    numb = 'положительное'
+    
+} 
+else (3 > 10) 
+console.log(numb);
+
+
+numb = 'отрицательное'
+
+const dayOfWeek = ("пн", "вт", "ср", "чт", "пт", "сб", "вс")
+
+switch (dayOfWeek) {
+case 'пн', 'вт', 'ср', 'чт', 'пт':
+ console.log("Рабочий день")
+ case 'сб' ,'вс':
+ console.log("Выходной")
+}
+
+
+
+
+
